2400fb8d197424069126fa1d98789000f557c8cb
Angelo Stavrow <contact@angelostavrow.com>
2020-08-13 11:59:18 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Angelo Stavrow on 2020-08-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SwiftUIPreferredColorSchemeExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPreferredColorSchemeExampleApp.swift
+//  Shared
+//
+//  Created by Angelo Stavrow on 2020-08-13.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPreferredColorSchemeExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
